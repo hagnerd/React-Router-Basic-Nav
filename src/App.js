bf03341ef1ffd96b33a421dba1ea2fc390f42e99
Matt Hagner <web@matthagner.com>
2019-06-19 18:46:39 -0500

    Add Routes to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import { Route } from "react-router-dom";
+import { Home, About, Contact, Navigation } from "./components";
+import "./App.css";
 
 const App = () => (
   <div>
     <Navigation />
+    <Route path="/" exact component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
